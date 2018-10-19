b2a4505e322cb448780c797cf16a541bacdf77ab
cs2jang <43163358+cs2jang@users.noreply.github.com>
2018-10-19 13:33:28 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,7 +1,7 @@
 from QeuryLoader import QueryLoader
 
 ql = QeuryLoader()
-query_format = ql.get_query("query", "get_test")
+query_format = ql.get_query("query", "get_name")  # get_query(xml file name, id tag name)
 query = query_format.format(NAME='test')
 
 print("Query String : ", query)
